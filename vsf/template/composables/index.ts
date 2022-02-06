18b2126efcee342ca82b271814684189aaf9c66a
Diogo Duarte <dduarte@odoogap.com>
2022-02-06 21:35:13 +0000

    [FIX] remove unused files

--- vsf/template/composables/index.ts
+++
@@ -1,9 +0,0 @@
-import useUiHelpers from './useUiHelpers';
-import useUiNotification from './useUiNotification';
-import useUiState from './useUiState';
-
-export {
-    useUiHelpers,
-    useUiNotification,
-    useUiState
-}
