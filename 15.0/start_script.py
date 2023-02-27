89b69a862b558e8a9f3ae01abd5954d0617e3cba
Diogo Duarte <dduarte@odoogap.com>
2023-02-27 22:02:27 +0000

    [FIX] use the same invalidation key

--- 15.0/start_script.py
+++ 15.0/start_script.py
@@ -1,4 +1,10 @@
 #!/bin/python
+import os
+
+
 env['ir.config_parameter'].set_param('web.base.url', 'http://localhost:8069')
 env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
+env['ir.config_parameter'].set_param('vsf_cache_invalidation_url', 'http://vsf:3000/cache-invalidate')
+env['ir.config_parameter'].set_param('vsf_cache_invalidation_key', os.getenv('INVALIDATION_KEY'))
+
 env.cr.commit()
