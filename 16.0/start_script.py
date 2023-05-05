d5692cf27440e555b31b1b6efb974678aa5af88e
Diogo Duarte <dduarte@odoogap.com>
2023-05-05 08:42:19 +0100

    [IMP] update last version

--- 16.0/start_script.py
+++ 16.0/start_script.py
@@ -6,5 +6,6 @@ env['ir.config_parameter'].set_param('web.base.url', 'http://localhost:8069')
 env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
 env['ir.config_parameter'].set_param('vsf_cache_invalidation_url', 'http://vsf:3000/cache-invalidate')
 env['ir.config_parameter'].set_param('vsf_cache_invalidation_key', os.getenv('INVALIDATION_KEY'))
+env['ir.config_parameter'].set_param('vsf_debug_mode', True)
 
 env.cr.commit()
