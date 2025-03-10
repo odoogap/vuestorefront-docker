e3b6ebb756f0d8cf4cc2b384547e65d99312a008
Diogo Duarte <dduarte@erpgap.com>
2025-03-10 21:43:27 +0000

    [FIX] add redis dependency and fix init script

--- 18.0/startup.sh
+++ 18.0/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-function install_vuestorefront() {
-    if [ ! -d "/mnt/extra-addons/graphql_vuestorefront" ];
+function install_alokai() {
+    if [ ! -d "/mnt/extra-addons/graphql_alokai" ];
     then 
         git clone --recurse-submodules --branch 18.0 https://github.com/erpgap/alokai-odoo.git /mnt/extra-addons/
         /entrypoint.sh odoo -d v18_odoo --max-cron-threads 0 --no-http --stop-after-init -i graphql_alokai
         /entrypoint.sh odoo shell -d v18_odoo --max-cron-threads 0 --stop-after-init --no-http < /start_script.py 
     fi
 }
-install_vuestorefront && /entrypoint.sh odoo
+install_alokai && /entrypoint.sh odoo
 exit 0
