97765f71baa4652c101a5526a7b77c0a0b5d8a28
Diogo Duarte <dduarte@erpgap.com>
2025-03-11 14:26:10 +0000

    [IMP] setting redis config for docker

--- 18.0/start_script.py
+++ 18.0/start_script.py
@@ -5,5 +5,8 @@ env['ir.config_parameter'].set_param('web.base.url', 'http://localhost:8069')
 env['ir.config_parameter'].set_param('web.base.url.freeze', 'True')
 env['ir.config_parameter'].set_param('vsf_cache_invalidation_url', 'http://vsf:3000/cache-invalidate')
 env['ir.config_parameter'].set_param('vsf_cache_invalidation_key', os.getenv('NUXT_REDIS_INVALIDATION_KEY'))
+env['ir.config_parameter'].set_param('alokai_redis_host', 'redis')
+env['ir.config_parameter'].set_param('alokai_redis_port', '6379')
+
 
 env.cr.commit()
