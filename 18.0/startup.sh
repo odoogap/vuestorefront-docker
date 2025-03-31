92c50a13b3093212cfba936c193958598de5534a
Diogo Duarte <dduarte@erpgap.com>
2025-03-31 17:46:14 +0100

    [IMP] bump up postgresql to version 16

--- 18.0/startup.sh
+++ 18.0/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+BRANCH="18.0"
 function install_alokai() {
     if [ ! -d "/mnt/extra-addons/graphql_alokai" ];
     then 
-        git clone --recurse-submodules --branch 18.0 https://github.com/erpgap/alokai-odoo.git /mnt/extra-addons/
+        git clone --recurse-submodules --branch $BRANCH https://github.com/erpgap/alokai-odoo.git /mnt/extra-addons/
         /entrypoint.sh odoo -d v18_odoo --max-cron-threads 0 --no-http --stop-after-init -i graphql_alokai
         /entrypoint.sh odoo shell -d v18_odoo --max-cron-threads 0 --stop-after-init --no-http < /start_script.py 
     fi
